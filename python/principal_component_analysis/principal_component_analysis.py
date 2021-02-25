2dc935e3e3a383edf8be2a7e845badb128efbb6c
Jaco de Swardt <31213557+jdeswardt@users.noreply.github.com>
2021-02-25 10:32:46 +0200

    [GJ] Finalizing script

--- python/principal_component_analysis/principal_component_analysis.py
+++ python/principal_component_analysis/principal_component_analysis.py
@@ -23,6 +23,9 @@ df_us_arrests = pandas.read_csv(filepath_or_buffer='~/Documents/GitHub/data_scie
                                        'population',
                                        'rape'])
 
+##Check dataframe
+print(df_us_arrests)
+
 ##Create dataframe with only the independent X variables
 df_features = df_us_arrests[['murder',
                              'assualt',
@@ -42,24 +45,21 @@ df_features.describe()
 pair_plots = plotly.express.scatter_matrix(data_frame=df_features)
 pair_plots.show()
 
-##Standardize features
+################################################################################
+#3.) IMPLEMENT PCA
+
+##3.1) Standardize features
 features_standardized = StandardScaler().fit_transform(df_features)
 df_features_standardized = pandas.DataFrame(data=features_standardized,
                                             columns=['murder',
                                                      'assualt',
                                                      'population',
                                                      'rape'])
+
+##Check dataframe
 print(df_features_standardized)
 
-################################################################################
-#X.) PCA BENEFITS FOR VISUALIZATION
-
-##Add pairplots
-
-################################################################################
-#X.) IMPLEMENT PCA
-
-##How many components do we need to retain
+##3.2) Create principal components for each feature
 ##Specify number of components, create p components for p features
 pca = PCA(n_components=4)
 
@@ -76,6 +76,7 @@ df_prin_comp = pandas.DataFrame(data=principal_components,
 ##Check dataframe
 print(df_prin_comp)
 
+##3.3) Evaluate number of components required
 ##Number of principal components
 pc_values = numpy.arange(pca.n_components_) + 1
 
@@ -86,42 +87,60 @@ variation_explained = pca.explained_variance_ratio_
 fig = plotly.express.line(x=pc_values,
                           y=variation_explained,
                           title='Scree plot',
-                          labels={'x' : 'Number of principal components',
-                                  'y' : 'Proportion of variance explained'})
+                          labels={'x': 'Number of principal components',
+                                  'y': 'Proportion of variance explained'})
 fig.update_traces(mode='markers+lines')
 fig.show()
 
-##Another way to show this
-##Calculate the cumulative variance
+##Calculate the cumulative variance explained
 cumul_var_explained = numpy.cumsum(pca.explained_variance_ratio_)
 
 ##Create a area plot
 fig = plotly.express.area(x=range(1, cumul_var_explained.shape[0] + 1),
                           y=cumul_var_explained,
-                          labels={'x' : 'Number of principal components',
-                                  'y' : 'Cumulative variance explained'})
+                          labels={'x': 'Number of principal components',
+                                  'y': 'Cumulative variance explained'})
 fig.update_traces(mode='markers+lines')
 fig.show()
 
 ################################################################################
-#X.) RESULTS
+#4.) PLOTLY EXPRESS BIPLOT
 
+##Create feature names list
+features = ['murder',
+            'assualt',
+            'population',
+            'rape']
 
+##Create dataframe showing the state
+state = df_us_arrests['state']
 
-##Specify number of components
-pca = PCA(n_components=2)
+##Calculate loadings
+loadings = pca.components_.T * numpy.sqrt(pca.explained_variance_)
 
-##Implement pca on standardized features
-principalComponents = pca.fit_transform(df_features_standardized)
+##Create scatter object
+biplot = plotly.express.scatter(data_frame=principal_components,
+                                x=0,
+                                y=1,
+                                text=state,
+                                title='Biplot for US arrests data')
 
+##Add vectors to plot
+for i, feature in enumerate(features):
+    biplot.add_shape(type='line',
+                     x0=0,
+                     y0=0,
+                     x1=loadings[i, 0],
+                     y1=loadings[i, 1])
+    biplot.add_annotation(x=loadings[i, 0],
+                          y=loadings[i, 1],
+                          ax=0,
+                          ay=0,
+                          xanchor='center',
+                          yanchor='bottom',
+                          text=feature)
 
-
-principalDf = pandas.DataFrame(data=principalComponents,
-                               columns=['principal_component_1',
-                                        'principal_component_2'])
-
-
-
-
+##Show plot
+biplot.show()
 
 ################################################################################
